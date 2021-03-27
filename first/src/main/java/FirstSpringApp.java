df728d9910d007b26286b67f5741e3d3065db753
devserg <devserg@example.com>
2021-03-27 13:06:56 +0000

    Spring app

---
+++ first/src/main/java/FirstSpringApp.java
@@ -0,0 +1,12 @@
+package ru.itsre;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class FirstSpringApp {
+
+        public static void main(String[] args) throws Exception {
+                SpringApplication.run(FirstSpringApp.class, args);
+                        }
+}
